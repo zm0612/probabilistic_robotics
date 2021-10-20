2e3a544e7d4a80c45db9b9d479c5eae88c0714e9
Zhang Zhimeng <zhangzhimeng@turingvideo.net>
2021-10-20 13:45:14 +0800

    1.Add the experimental results of linear Kalman Filter

--- lecture_3_gaussian_filter/code/kalman_filter_example.cpp
+++ lecture_3_gaussian_filter/code/kalman_filter_example.cpp
@@ -1,7 +1,17 @@
+#include "matplotlibcpp.h"
+
 #include <vector>
 #include <random>
 #include <iostream>
 #include <Eigen/Dense>
+#include <Eigen/SVD>
+
+namespace plt = matplotlibcpp;
+
+void AnalyzeObservability(const Eigen::MatrixXd Q) {
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(Q);
+    std::cout << "eigen value: " << svd.singularValues().eval().transpose() << std::endl;
+}
 
 std::vector<double> GetSimData(double init_p, double init_v, double init_a,
                                double duration, double noise, double period,
@@ -16,7 +26,7 @@ std::vector<double> GetSimData(double init_p, double init_v, double init_a,
 
     double p_k;
     for (unsigned int i = 0; i < N; ++i) {
-        p_k = init_p + init_v * (i * period) + 0.5 * init_a * std::pow(period, 2.0);
+        p_k = init_p + init_v * (i * period) + 0.5 * init_a * std::pow(period * i, 2.0);
         ground_truth.emplace_back(p_k);
         p_k = p_k + normal_distribution(gen);
         positions.emplace_back(p_k);
@@ -28,9 +38,9 @@ std::vector<double> GetSimData(double init_p, double init_v, double init_a,
 void KalmanFilter() {
     double init_p = 3.5;
     double init_v = 1.3;
-    double init_a = 0.05;
-    double measure_noise = 0.2;
-    double duration = 100;
+    double init_a = 0.1;
+    double measure_noise = 10;
+    double duration = 60;
     double T = 1.0;
     std::vector<double> ground_truth;
     std::vector<double> measures = GetSimData(init_p, init_v, init_a, duration,
@@ -51,6 +61,11 @@ void KalmanFilter() {
     B << 0.5 * T * T, T;
     C << 1, 0;
 
+    std::vector<double> filter_p;
+    std::vector<double> filter_v;
+    std::vector<double> t;
+    std::vector<double> gt_v;
+    std::vector<double> gt_p;
     auto N = static_cast<unsigned int >(duration / T);
     for (unsigned int i = 0; i < N; ++i) {
         state = A * state + B * init_a;
@@ -60,7 +75,35 @@ void KalmanFilter() {
         state = state + K * (measures[i] - C * state);
         Sigma = (Eigen::Matrix2d::Identity() - K * C) * Sigma;
         std::cout << "iter: " << i << " state: " << state.transpose() << " | gt(p): " << ground_truth[i] << std::endl;
+        filter_p.emplace_back(state[0]);
+        filter_v.emplace_back(state[1]);
+        t.emplace_back(i);
+        double gt_v_k = init_v + init_a * (i * T);
+        gt_v.emplace_back(gt_v_k);
+        double gt_p_k = init_p + init_v * (i * T) + 0.5 * init_a * std::pow((i * T), 2);
+        gt_p.emplace_back(gt_p_k);
     }
+
+    plt::suptitle("Kalman Filter");
+    plt::subplot2grid(2, 1, 0, 0);
+    plt::plot(t, filter_p, "r");
+    plt::plot(t, measures, "b");
+    plt::plot(t, gt_p, "g");
+    plt::named_plot("gt_p", t, gt_p);
+    plt::named_plot("measure_p", t, measures);
+    plt::named_plot("filtered_p", t, filter_p);
+    plt::title("position");
+    plt::legend();
+
+    plt::subplot2grid(2, 1, 1, 0);
+    plt::plot(t, gt_v, "g");
+    plt::plot(t, filter_v, "r");
+    plt::named_plot("gt_v", t, gt_v, "g");
+    plt::named_plot("filtered_v", t, filter_v, "r");
+    plt::title("velocity");
+    plt::legend();
+
+    plt::show();
 }
 
 int main() {
---
+++ lecture_3_gaussian_filter/readme.md
@@ -0,0 +1,86 @@
+# 实验报告
+
+> 在绘制图形时使用了：[matplotlib-cpp](https://github.com/lava/matplotlib-cpp)
+>
+> 需要安装依赖：sudo apt-get install python-matplotlib python-numpy python2.7-dev
+
+## 1. 线性卡尔曼滤波
+
+### 1.1 运动模型
+
+为了深入了解卡尔曼滤波，我通过代码实现了一个线性卡尔曼滤波的过程。
+
+其运动模型是一个拥有初速度，并且以恒定加速度直线运动的系统。
+
+状态转移矩阵如下：
+$$
+x_t  = A  x_{t-1} + B * u
+$$
+测量模型如下：
+$$
+y_t = Cx_t
+$$
+带入上述运动系统中：
+$$
+\left[
+\begin{matrix}
+ \hat p_t \\ \hat v_t
+\end{matrix}
+\right]
+=
+\left[
+\begin{matrix}
+1 & T \\ 0 & 1
+\end{matrix}
+\right]
+\left[
+\begin{matrix}
+p_{t-1} \\ v_{t-1}
+\end{matrix}
+\right]
++
+\left[
+\begin{matrix}
+0.5 T^2 \\ T
+\end{matrix}
+\right]
+\left[
+\begin{matrix}
+a
+\end{matrix}
+\right]
+$$
+
+$$
+\left[
+\begin{matrix}
+y_t
+\end{matrix}
+\right]
+=
+\left[
+\begin{matrix}
+1 & 0
+\end{matrix}
+\right]
+\left[
+\begin{matrix}
+\hat p_t \\ \hat v_t
+\end{matrix}
+\right]
+$$
+
+### 1.2 实验结果
+
+> 对应的源码在：`lecture_3_gaussian_filter/code/kalman_filter_example.cpp`
+
+**位移**
+
+![kalman_filter_0](doc/kalman_filter_0.png)
+
+**速度**
+
+![kalman_filter_1](doc/kalman_filter_1.png)
+
+**结论**：通过上图可以发现在状态量为位移和速度，而观测量为位移时，位移的状态可以很快收敛，并且最终在一个较小的范围波动。但是对于上面的例子，速度在刚开始的时候收敛的比较快。
+
